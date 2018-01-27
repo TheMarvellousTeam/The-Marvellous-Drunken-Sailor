08f2b81b592066d4b43ee40327ac698bbd0b5e8e
platane <me@platane.me>
2018-01-27 13:48:51 +0100

    grid

--- packages/client/src/renderer/grid/index.js
+++ packages/client/src/renderer/grid/index.js
@@ -7,22 +7,49 @@ export const init = scene => {
   container.name = 'ground'
   container.position.z = 0
 
-  const geo = new THREE.PlaneBufferGeometry(1, 1)
+  const canvas = document.createElement('canvas')
+  canvas.width = canvas.height = 2
+  const ctx = canvas.getContext('2d')
+  ctx.fillStyle = 'rgba(0,0,0,0.3)'
+  ctx.beginPath()
+  ctx.rect(0, 0, 1, 1)
+  ctx.fill()
+  ctx.beginPath()
+  ctx.rect(1, 1, 1, 1)
+  ctx.fill()
+
+  const L = 200
+
+  const texture = new THREE.Texture(canvas)
+  texture.needsUpdate = true
+  texture.magFilter = texture.minFilter = THREE.NearestFilter
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  texture.repeat.set(L / 2, L / 2)
+
+  const geo = new THREE.PlaneBufferGeometry(L, L)
   const mat = new THREE.MeshBasicMaterial({
-    color: 0xf41f2f,
+    map: texture,
+    // color: 0xf41f2f,
     transparent: true,
-    opacity: 0.4,
+    // opacity: 0.4,
   })
 
-  for (let x = -10; x < 10; x += 1)
-    for (let y = -10 + x % 2; y < 10; y += 2) {
-      const mesh = new THREE.Mesh(geo, mat)
+  const mesh = new THREE.Mesh(geo, mat)
+  scene.add(mesh)
 
-      mesh.position.x = x
-      mesh.position.y = y
+  mesh.position.x = 0.5
+  mesh.position.y = 0.5
 
-      container.add(mesh)
-    }
+  //
+  // for (let x = -10; x < 10; x += 1)
+  //   for (let y = -10 + x % 2; y < 10; y += 2) {
+  //     const mesh = new THREE.Mesh(geo, mat)
+  //
+  //     mesh.position.x = x
+  //     mesh.position.y = y
+  //
+  //     container.add(mesh)
+  //   }
 
   // var geometry = new THREE.BoxGeometry(1, 1, 1)
   // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })

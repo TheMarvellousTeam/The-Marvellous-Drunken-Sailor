a399936efef072e51d2f69916c70818b4a3ba363
NiZiL <biasutto.t@gmail.com>
2018-01-28 16:16:28 +0100

    Splash screen and lobby !

--- packages/client/src/ui/Lobby/index.js
+++ packages/client/src/ui/Lobby/index.js
@@ -8,13 +8,19 @@ const randomRoomName = () =>
 
 export const Lobby = ({ roomId, started, connecting }) => (
   <Container>
-    <div style="background-image:url('./logo/logo.png');position:fixed;top:0;left:0;bottom:0;right:0"></div>
+    <div class="gradient">
+    <div class="center">
+      <div class="logo"></div>
+      {!roomId && <a class="playButton" href={`?roomId=${randomRoomName()}`}>Play</a>}
+    
     {connecting && <span>{`connecting to ${roomId}`}</span>}
+    
+    {!connecting && roomId && <span>{`waiting for another player in ${roomId}`}</span>}
 
-    {!connecting &&
-      roomId && <span>{`waiting for another player in ${roomId}`}</span>}
+      <br/> A<a href="https://github.com/TheMarvellousTeam/">Marvellous Team</a>production
+    </div>
+    </div>
 
-    {!roomId && <a href={`?roomId=${randomRoomName()}`}>create room</a>}
   </Container>
 )
 
